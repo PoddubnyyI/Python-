d1431f1916224d4c2b1d6d42c341d91b5833c6d9
Поддубный Илья <148816077+PoddubnyyI@users.noreply.github.com>
2024-07-17 00:34:07 +0300

    Add files via upload

---
+++ Бла. Py.py
@@ -0,0 +1,13 @@
+favorite_languages = { 
+    'Phil': 'phyton', 
+    'Ben': 'C++',
+    'Jack': 'phyton',
+    'Mike': 'Go', 
+    'Edward': 'ruby', 
+    }
+    
+print(f"Список всех участников {favorite_languages}")
+
+name = input('Введите имя\n')
+
+print(favorite_languages['Ben'])
